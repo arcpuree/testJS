af6ca5ae62cd7f5056e6f78c092b283de985387b
arcpuree <arc.puree@gmail.com>
2018-09-17 21:48:04 +0700

    test

---
+++ getJsonByURL/index.js
@@ -0,0 +1,14 @@
+var request = require("request");
+var http = require('http');
+
+var url = "https://api.data.go.th/search_virtuoso/api/dataset/query?dsname=vir_231&path=vir_231&property=col_4&operator=CONTAINS&valueLiteral=%E0%B8%9E%E0%B8%B4%E0%B8%9E%E0%B8%B4%E0%B8%98%E0%B8%A0%E0%B8%B1%E0%B8%93%E0%B8%91%E0%B9%8C&property=col_8&operator=CONTAINS&valueLiteral=%E0%B8%99%E0%B9%88%E0%B8%B2%E0%B8%99&loadAll=1&type=json&limit=100&offset=0"
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+    }
+})
